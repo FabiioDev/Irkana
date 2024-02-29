67b66022df67175a4ef8014238aa522698af88e1
hawokdev <rodolfovargasoff@gmail.com>
2024-02-28 19:50:54 -0600

    All until part 20, working with some examples of 'vistas basadas en clase'

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,4 +1,6 @@
+
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from core.erp.models import Category
 
@@ -8,5 +10,14 @@ def category_list(request):
         'title': 'Listado de Categorías',
         'categories': Category.objects.all()
     }
-
     return render(request, 'category/list.html', data)
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de Categorías'
+        return context
