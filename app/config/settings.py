d39b536d147edbe8e3410569aaa4ae94adeb60ae
fabio-SwDev <faboordo@gmail.com>
2024-03-04 21:52:29 -0600

    created the base form for category

--- app/config/settings.py
+++ app/config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Libs
+    'widget_tweaks',
     # Apps
     'core.erp',
 ]
